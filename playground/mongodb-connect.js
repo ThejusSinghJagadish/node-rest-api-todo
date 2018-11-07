9ca85fdb4aa6b01514b01e052e3af84f62951859
thejus <thejussingh.jagadish@gmail.com>
2018-11-06 19:43:37 -0800

    Add GET /todos route

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -9,26 +9,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     return console.log('Error : ', 'Unable to connect to MongoDb Server');
   }
   console.log('Success : ', 'Connection to MongoDb Server Established');
-  // db.collection('Todos').insertOne({
-  //   text: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert Todo');
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  db.collection('Todos').insertOne({
+    text: 'Something to do',
+    completed: false
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to insert Todo');
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
 
-  // db.collection('Users').insertOne({
-  //   _id: 123,
-  //   name: 'Sanath',
-  //   age: 26,
-  //   location: 'Santa Clara'
-  // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert User');
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+  db.collection('Users').insertOne({
+    _id: 123,
+    name: 'Sanath',
+    age: 26,
+    location: 'Santa Clara'
+  }, (err, result) => {
+    if(err){
+      return console.log('Unable to insert User');
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
   db.close();
 });
